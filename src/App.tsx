4b6cebe826e34e4242d634962cef4fa4457876ec
Andrija Pavicevic <89163400+Andrija2504@users.noreply.github.com>
2024-11-10 10:28:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,19 @@ import PostsPage from './components/PostsPage';
 import ProfilePage from './components/ProfilePage';
 import AvailableGamesPage from './components/AvailableGamesPage';
 import GameDetailPage from './components/GameDetailPage';
+interface AppProps {
+    isWebXrSupported: boolean;
+} 
 
-const App: React.FC = () => {
+const App: React.FC<AppProps> = ({isWebXrSupported  }) => {
   const loggedInUserId = 2;
 
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<PostsPage loggedInUserId={loggedInUserId} />} />
-        <Route path="/profile" element={<ProfilePage loggedInUserId={loggedInUserId} />} />
+        <Route path="/" element={<PostsPage loggedInUserId={loggedInUserId} isWebXrSupported={isWebXrSupported}/>} />
+        <Route path="/profile" element={<ProfilePage loggedInUserId={loggedInUserId} isWebXrSupported={isWebXrSupported}/>} />
         <Route path="/games" element={<AvailableGamesPage loggedInUserId={loggedInUserId} />} />
         <Route path="/games/:gameId" element={<GameDetailPage />} /> {/* New route */}
       </Routes>
